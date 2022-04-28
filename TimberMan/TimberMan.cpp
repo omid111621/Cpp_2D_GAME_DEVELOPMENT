cf016912d85e71938eef5d2d02aea2cded327395
omid111621 <omid111621@gmail.com>
2022-04-28 21:36:58 +0430

    second commit

--- TimberMan/TimberMan.cpp
+++ TimberMan/TimberMan.cpp
@@ -30,8 +30,8 @@ int main()
 	* This way we only need to load a graphic once!
 	* Everything is going to be a Sprite, even the background.
 	*/
-	Texture backgroundTexture;
-	// Setting up our background.
+
+	// Setting up the background.
 	//step 1.
 	Texture backgroundTextureObject;
 	//step 2.
@@ -43,7 +43,57 @@ int main()
 	//Set the position of backgroundSpritebject in order to cover the entire sceen.
 	backgroundSpriteObject.setPosition(0, 0);
 
+	// Setting up the tree.
+	//step 1.
+	Texture treeTextureObject;
+	//step 2.
+	treeTextureObject.loadFromFile("graphics/tree.png");
+	//step 3.
+	Sprite treeSpriteObject;
+	//step 4.
+	treeSpriteObject.setTexture(treeTextureObject);
+	//Set the position of treeSpritebject in order to cover the entire sceen.
+	treeSpriteObject.setPosition(810, 0);
 
+	// Setting up the bee.
+	//step 1.
+	Texture beeTextureObject;
+	//step 2.
+	beeTextureObject.loadFromFile("graphics/bee.png");
+	//step 3.
+	Sprite beeSpriteObject;
+	//step 4.
+	beeSpriteObject.setTexture(beeTextureObject);
+	//Set the position of treeSpritebject in order to cover the entire sceen.
+	beeSpriteObject.setPosition(0, 800);
+	// The bee's activity parameters.
+	bool beeActivityStatus = false;
+	float beeSpeed = 0.0f;
+	
+	// Setting up the 3 clouds.
+	//step 1.
+	Texture cloudTextureObject;
+	//step 2.
+	cloudTextureObject.loadFromFile("graphics/cloud.png");
+	//step 3.
+	Sprite cloudOneSpriteObject;
+	Sprite cloudTwoSpriteObject;
+	Sprite cloudThreeSpriteObject;
+	//step 4.
+	cloudOneSpriteObject.setTexture(cloudTextureObject);
+	cloudTwoSpriteObject.setTexture(cloudTextureObject);
+	cloudThreeSpriteObject.setTexture(cloudTextureObject);
+	//Set the position of treeSpritebject in order to cover the entire sceen.
+	cloudOneSpriteObject.setPosition(0, 0);
+	cloudTwoSpriteObject.setPosition(0, 250);
+	cloudThreeSpriteObject.setPosition(0, 500);
+	// The clouds' activity parameters.
+	bool cloudOneActivityStatus = false;
+	bool cloudTwoActivityStatus = false;
+	bool cloudThreeActivityStatus = false;
+	float cloudOneSpeed = 0.0f;
+	float cloudTwoSpeed = 0.0f;
+	float cloudThreeSpeed = 0.0f;
 	//Starting the Game Loop.
 	while (window_object.isOpen()) {
 
@@ -70,13 +120,19 @@ int main()
 		***************************************
 		*/
 
-		//	Clear everything from the last frame.    //
-		window_object.clear();                       //
-													 // ____________ This process is called DOUBLE BUFFERING
-		//	Draw our game background here.           // 
-		window_object.draw(backgroundSpriteObject);  //
-		//	Show everything we just drew.            //
-		window_object.display();                     //
+		//	Clear everything from the last frame.      //                                                                                          //
+		window_object.clear();                         //                                                                                          //
+													   //                                                                                          //
+		//	Draw the game's scene here.                //                                                                                          //
+		window_object.draw(backgroundSpriteObject);    //                                                                                          //
+		window_object.draw(treeSpriteObject);	       //Note, however, the order in which we draw the different                                   //
+		window_object.draw(treeSpriteObject);	       //objects to the screen.We must draw all of the graphics after the background, or they      //____________ This process is called DOUBLE BUFFERING
+		window_object.draw(beeSpriteObject);	       //will be covered, and we must draw the clouds before the tree, or they will look a bit     //
+	    window_object.draw(cloudOneSpriteObject);	   //odd drifting in front of the tree.The bee would look OK either in front or behind         //
+		window_object.draw(cloudTwoSpriteObject);	   //the tree.I opted to draw the bee in front of the tree so that it can try and distract our //
+		window_object.draw(cloudThreeSpriteObject);	   //lumberjack, a bit like a real bee might.                                                  //
+		//	Show everything we just drew.              //                                                                                          //
+		window_object.display();                       //                                                                                          //
 
 	}
 
